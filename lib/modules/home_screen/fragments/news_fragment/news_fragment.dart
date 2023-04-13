6f9c8cafd7c2ffeddb0a835c180b298e8dfeb3e6
alfinramadhana <alfinramadhana07@gmail.com>
2023-04-13 11:47:33 +0700

    update news fragment

--- lib/modules/home_screen/fragments/news_fragment/news_fragment.dart
+++ lib/modules/home_screen/fragments/news_fragment/news_fragment.dart
@@ -5,9 +5,29 @@ class NewsFragment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("News Fragment"),
+    return Scaffold(
+      appBar: AppBar(
+        title: new Center(
+          child: Text('News App', style: TextStyle(color: Colors.black))),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Container(
+              color: Colors.grey[200],
+              height: 100,
+              width: 100,
+            ),
+            title: Text(
+              'Title',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            onTap: () {
+            },
+          );
+        },
       ),
     );
   }
